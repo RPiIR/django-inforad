0671e79c567ce8731213d047d20b74af0ff47b26
Milan Dunghubel <milan.dunghubel@zoomint.com>
2014-10-20 00:29:12 +0200

    Initial commit

---
+++ requirements.txt
@@ -0,0 +1,4 @@
+djangorestframework
+markdown
+django-filter
+
---
+++ webui/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+
+from rest_framework import routers
+from radiator import views
+
+from django.contrib import admin
+admin.autodiscover()
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'webui.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
