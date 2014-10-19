056819153ba7cd100f5d645b70f0ad1abf429175
Milan Dunghubel <milan.dunghubel@zoomint.com>
2014-10-20 01:10:25 +0200

    Update

--- radiator/admin.py
+++ radiator/admin.py
@@ -1,3 +1,28 @@
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
+from radiator.models import Alarm
+
+class AlarmForm(forms.ModelForm):
+    pass
+    #if conf.TWITTER_ACCESS_TOKEN and conf.TWITTER_ACCESS_SECRET:
+    #    post_to_twitter = forms.BooleanField(required=False, label="Post to Twitter", help_text="This will send a tweet with a brief summary, the permalink to the event (if BASE_URL is defined), and the hashtag of #status for EACH update you add below.")
+
+    #class Meta:
+    #    model = AlarmUpdate
+
+class AlarmAdmin(admin.ModelAdmin):
+    #form = AlarmForm
+    #list_display = ('date_created', 'description', 'status', 'date_updated')
+    search_fields = ('description', 'message')
+    #list_filter = ('services',)
+    #inlines = [AlarmUpdateInline]
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save()
+        if 'post_to_twitter' in form.cleaned_data and form.cleaned_data['post_to_twitter']:
+            for obj in instances:
+                obj.event.post_to_twitter(obj.get_message())
+
+admin.site.register(Alarm, AlarmAdmin)
--- radiator/models.py
+++ radiator/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import datetime
+
+STATUS_CHOICES = (
+    (0, 'No Problems'),
+    (1, 'Some Issues'),
+    (2, 'Unavailable'),
+)
 
 # Create your models here.
+class Alarm(models.Model):
+    slug = models.SlugField(max_length=128, unique=True)
+    message = models.TextField(null=True, blank=True, editable=False)
+    description = models.TextField(null=True, blank=True, help_text='We will auto fill the description from the first event message if not set')
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, editable=False, default=0)
+    order = models.IntegerField(default=0)
+    date_created = models.DateTimeField(default=datetime.datetime.now, editable=False)
+    date_updated = models.DateTimeField(default=datetime.datetime.now, editable=False)
+
+    class Meta:
+        ordering = ('order', 'date_created')
+
+    def __unicode__(self):
+        if self.message:
+            return self.message
+        else:
+            return self.slug
+
