02451810e67e51a1cfc212ac6b684b2dbcefb5e0
Milan Dunghubel <milan.dunghubel@zoomint.com>
2014-10-20 06:43:28 -0400

    Create exchange on amqp

--- radiator/device.py
+++ radiator/device.py
@@ -6,6 +6,8 @@ class Device:
         self.producer = puka.Client("amqp://127.0.0.1/")
         send_promise = self.producer.connect()
         self.producer.wait(send_promise)
+        exchange_promise = self.producer.exchange_declare(exchange='radiator', type='topic')
+        self.producer.wait(exchange_promise)
 
     def _send(self,cmd):
         send_promise = self.producer.basic_publish(exchange='radiator', routing_key='radiator', body=cmd)
